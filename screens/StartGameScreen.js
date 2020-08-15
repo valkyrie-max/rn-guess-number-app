9790219be71aa795cfd4d3cd647da2838339059a
valkyrie-max <alisakitkina2016@gmail.com>
2020-08-15 17:34:55 -0500

    more input validating; added resetting and confirming functionality to the input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -8,12 +8,35 @@ import Input from '../components/Input'
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('')
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState()
 
     const numberInputHandler = inputText => {
         setEnteredValue(
-            // regex to validate the input text
+            // regex to not allow the user enter decimals
             inputText.replace(/[^0-9]/g, '')
         );
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue('')
+        setConfirmed(false)
+    }
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(chosenNumber);
+        setEnteredValue(''); 
+    }
+
+
+    let confirmedOutput; 
+    if (confirmed) {
+        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
     }
 
     return (
@@ -38,13 +61,22 @@ const StartGameScreen = props => {
                     {/* buttons */}
                     <View style={styles.buttonContainer}>
                         <View style={styles.buttonView}>
-                            <Button color={Colors.primary} title="Reset" onPress={() => {}}/>
+                            <Button 
+                                color={Colors.primary} 
+                                title="Reset" 
+                                onPress={resetInputHandler}
+                            />
                         </View>
                         <View style={styles.buttonView}>
-                            <Button color={Colors.accent} title="Confirm" onPress={() => {}}/>    
+                            <Button 
+                                color={Colors.accent} 
+                                title="Confirm" 
+                                onPress={confirmInputHandler}
+                            />    
                         </View>
                     </View>
                 </Card>
+                {confirmedOutput}
             </View>
         </TouchableWithoutFeedback>
     )

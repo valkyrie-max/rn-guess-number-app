3f4c8a99b4155ae4729b9d0a275ff0902dd75aa8
valkyrie-max <alisakitkina2016@gmail.com>
2020-08-16 23:28:13 -0500

    added random number generator logic

---
+++ screens/GameScreen.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const generateRandomNumber = (min, max, exclude) => {
+    min = Math.ceil(min); 
+    max = Math.floor(max);
+    const randomNumber = Math.floor(Math.random() * (max-min)) + min; 
+    if (randomNumber === exclude) {
+        return generateRandomNumber(min, max, exclude)
+    } else {
+        return randomNumber; 
+    }
+}
+
+const GameScreen = props => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomNumber(1, 100, props.userChoice))
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default GameScreen; 

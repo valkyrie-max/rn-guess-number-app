e91878b0de6905e3ee00bf4817aca61c62c70a05
valkyrie-max <alisakitkina2016@gmail.com>
2020-08-22 18:16:28 -0500

    finished the game logic; added the button that restarts the game; styled the game over screen

--- App.js
+++ App.js
@@ -10,9 +10,13 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0)
 
+  const configureNewGameHandler = () => {
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber); 
-    setGuessRounds(0)
   }
 
   const gameOverHandler = (numOfRounds) => {
@@ -24,7 +28,7 @@ export default function App() {
   if (userNumber && guessRounds <= 0) {
     content =  <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
   } else if (guessRounds > 0) {
-    content = <GameOver />
+    content = <GameOver onRestart={configureNewGameHandler} roundsNumber={guessRounds} userNumber={userNumber} />
   }
 
   return (
--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
+
+import Card from '../components/Card'
+import Colors from '../constants/colors'
 
 const GameOver = props => {
     return(
         <View style={styles.screen}>
-            <Text>Game is over!</Text>
+            <Card style={styles.summaryContainer}>
+                <Text style={styles.gameOverText}>Game is over!</Text>
+                <View style={styles.numbers}>
+                    <Text style={styles.generalText}>Number you picked:</Text>
+                    <View style={styles.specificNumberContainer}>
+                        <Text style={styles.specificNumber}>{props.userNumber}</Text>
+                    </View>
+                </View>
+                <View style={styles.numbers}> 
+                    <Text style={styles.generalText}>Number of rounds:</Text>
+                    <View style={styles.specificNumberContainer}>
+                        <Text style={styles.specificNumber} >{props.roundsNumber}</Text>
+                    </View>
+                </View>
+                <View style={styles.restartButton} >
+                    <Button color="black" title="Restart?" onPress={props.onRestart} />
+                </View>
+            </Card>
         </View>
     )
 }
@@ -12,8 +32,42 @@ const GameOver = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
+        marginTop: 50,
+        alignItems: 'center',
+    },
+    summaryContainer: {
+        width: 350,
+        maxWidth: '80%',
         alignItems: 'center'
+    },
+    gameOverText: {
+        fontSize: 30,
+        marginBottom: 20
+    },
+    generalText: {
+        fontSize: 18,
+        marginBottom: 5
+    },
+    numbers: {
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    specificNumberContainer: {
+        borderWidth: 3,
+        borderColor: Colors.primary,
+        borderRadius: 15,
+        width: 100,
+        alignItems: 'center',
+        marginVertical: 10,
+        padding: 10
+    },
+    specificNumber: {
+        fontSize: 30,
+        fontWeight: '700',
+        color: Colors.primary,
+    },
+    restartButton: {
+        paddingTop: 20
     }
 })
 
